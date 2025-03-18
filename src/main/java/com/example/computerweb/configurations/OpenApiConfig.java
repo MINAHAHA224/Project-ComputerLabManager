93d775114ba2ef2c191f6ca74c69853a80659c08
MINAHAHA224 <144884804+MINAHAHA224@users.noreply.github.com>
2025-03-18 16:23:36 +0700

    update swagger UI

--- src/main/java/com/example/computerweb/configurations/OpenApiConfig.java
+++ src/main/java/com/example/computerweb/configurations/OpenApiConfig.java
@@ -27,19 +27,23 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         // dung de thang swagger no generate dung cai api neu ko setting no se mac dinh la http chu ko phai la https
         // co 2 sever de sai
-        List<Server> listServer = new ArrayList<>();
 
-        Server serverProduction = new Server();
-        serverProduction.setUrl("https://project-computerlabmanager-production.up.railway.app");
-        serverProduction.setDescription("Production server");
-        Server serverLocal = new Server();
-        serverLocal.setUrl("http://localhost:8080");
-        serverLocal.setDescription("Dev local server");
-        listServer.add(serverProduction);
-        listServer.add(serverLocal);
+        Server server = new Server();
+        server.setUrl("https://project-computerlabmanager-production.up.railway.app");
+        server.setDescription("Production server");
+//        List<Server> listServer = new ArrayList<>();
+//
+//        Server serverProduction = new Server();
+//        serverProduction.setUrl("https://project-computerlabmanager-production.up.railway.app");
+//        serverProduction.setDescription("Production server");
+//        Server serverLocal = new Server();
+//        serverLocal.setUrl("http://localhost:8080");
+//        serverLocal.setDescription("Dev local server");
+//        listServer.add(serverProduction);
+//        listServer.add(serverLocal);
 
         return new OpenAPI()
-                .servers(listServer)
+                .addServersItem(server)
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth",
                         new SecurityScheme()
