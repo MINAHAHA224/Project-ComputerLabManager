a53a0743faf7a1048205eced56bc2cfe66a8f9cc
MINAHAHA224 <144884804+MINAHAHA224@users.noreply.github.com>
2025-06-06 15:59:38 +0700

    Config CORS accept all API

--- src/main/java/com/example/computerweb/configurations/CorsConfig.java
+++ src/main/java/com/example/computerweb/configurations/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
